b3599c2e1fe19d327814035aa28fa928042264c9
liudianfei3 <liudianfei3@jd.com>
2020-07-29 10:50:54 +0800

    stream_test

---
+++ src/com/liudf/lambda/User.java
@@ -0,0 +1,20 @@
+package com.liudf.lambda;
+
+/**
+ * @ClassName User
+ * @Description User
+ * @Author liudianfei3
+ * @Date 2020/7/28 11:42
+ * @Version 1.0
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
